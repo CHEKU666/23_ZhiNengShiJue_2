463395e231110a4945e53363931710cb3c68d078
李云开 <474769828@qq.com>
2023-02-22 23:12:42 +0800

    编码器端口配置，设置PIT通道周期获取编码器数据

--- Project/project/user/src/main.c
+++ Project/project/user/src/main.c
@@ -38,46 +38,86 @@
 // 打开新的工程或者工程移动了位置务必执行以下操作
 // 第一步 关闭上面所有打开的文件
 // 第二步 project->clean  等待下方进度条走完
-
 // 本例程是开源库移植用空工程
 
 
-
+/*定义四个编码器*/
+int16 encoder1;
+int16 encoder2;
+int16 encoder3;
+int16 encoder4;
 int main(void)
 {
-    clock_init(SYSTEM_CLOCK_600M);  // 不可删除
-    debug_init();                   // 调试端口初始化
+	DisableGlobalIRQ();                                         //初始化阶段关闭总中断
+    clock_init(SYSTEM_CLOCK_600M);  							// 不可删除
+    debug_init();                   							// 调试端口初始化
+	system_delay_ms(300);         								//延时等待外设上电
 
-    gpio_init(B9,GPO,0,GPO_PUSH_PULL);
-    // 此处编写用户代码 例如外设初始化代码等
-    func_soft_delay(300);
-		tft180_init();
-		tft180_show_string (0, 0, "TFT180 Init!");
-		if(mpu6050_init()){tft180_show_string (0, 0, "MPU6050 Init Failure");}	
-		else tft180_show_string (0, 0, "MPU6050 Init OK");
-    // 此处编写用户代码 例如外设初始化代码等
-    while(1)
+	tft180_init();												//屏幕初始化
+	tft180_show_string (0, 0, "TFT180Init!");
+	if(mpu6050_init()==1){										//MPU陀螺仪初始化结果判断
+		tft180_show_string (0, 1, "MPU6050Fail");
+	}	
+	else tft180_show_string (0, 1, "MPU6050InitOK");
+	encoder_dir_init(QTIMER1_ENCODER1, QTIMER1_ENCODER1_CH1_C0, QTIMER1_ENCODER1_CH2_C1);    //初始化四路编码器
+	encoder_dir_init(QTIMER1_ENCODER2, QTIMER1_ENCODER2_CH1_C2, QTIMER1_ENCODER2_CH2_C24);
+	encoder_dir_init(QTIMER2_ENCODER1, QTIMER2_ENCODER1_CH1_C3, QTIMER2_ENCODER2_CH2_C25);
+	encoder_dir_init(QTIMER2_ENCODER2, QTIMER3_ENCODER2_CH1_B18, QTIMER3_ENCODER2_CH2_B19);
+	pit_ms_init(PIT_CH1,5);                                    //初始化pit通道1，周期为5毫秒，采集编码器数据
+	EnableGlobalIRQ(0);										   // 总中断最后开启
+	// 此处编写用户代码 例如外设初始化代码等
+	while(1)
     {
-        gpio_set_level(B9,1);
-        system_delay_ms(100);
-        gpio_set_level(B9,0);
-        system_delay_ms(100);
-        // gpio_set_level(B9,1)
         // 此处编写需要循环执行的代码
-        mpu6050_get_acc();                                     // 获取 MPU6050 加速度计数据
-				mpu6050_get_gyro();																		 // 获取 MPU6050 陀螺仪数据
-			   
-			  tft180_show_int (0,1, mpu6050_gyro_x,4 );
-			  tft180_show_int (0,2, mpu6050_gyro_y,4);
-			  tft180_show_int (0,3, mpu6050_gyro_z,4);
-			  tft180_show_int (0,4, mpu6050_acc_x,4);
-			  tft180_show_int (0,5, mpu6050_acc_y,4);
-			  tft180_show_int (0,6, mpu6050_acc_z,4);
-      
+		pit_enable(PIT_CH1);									// pit通道1开始计时
+		mpu6050_get_acc(); 										// 获取 MPU6050 加速度
+		mpu6050_get_gyro();										// 获取 MPU6050 陀螺仪数据
+	
+		tft180_show_int (0,2, mpu6050_gyro_x,5);
+		tft180_show_int (0,3, mpu6050_gyro_y,5);
+		tft180_show_int (0,4, mpu6050_gyro_z,5);
+		tft180_show_int (0,5, mpu6050_acc_x,5);
+		tft180_show_int (0,6, mpu6050_acc_y,5);
+		tft180_show_int (0,7, mpu6050_acc_z,5);
+		
         // 此处编写需要循环执行的代码
     }
 }
 
 
+void PIT_IRQHandler(void)
+{
+	if (PIT_FLAG_GET(PIT_CH0))
+	{
+		
+		pit_flag_clear(PIT_CH0);							//清除通道1中断标志位
+	}
 
+	if (PIT_FLAG_GET(PIT_CH1))
+	{
+		//通道1发生中断,开始采集（刷新）编码器数据
+		encoder1 = encoder_get_count(QTIMER1_ENCODER1);
+		encoder1 = encoder_get_count(QTIMER1_ENCODER2);
+		encoder1 = encoder_get_count(QTIMER2_ENCODER1);
+		encoder1 = encoder_get_count(QTIMER2_ENCODER2);
+		encoder_clear_count(QTIMER1_ENCODER1);
+		encoder_clear_count(QTIMER1_ENCODER2);
+		encoder_clear_count(QTIMER2_ENCODER1);
+		encoder_clear_count(QTIMER2_ENCODER2);
+		pit_flag_clear(PIT_CH1);
+	}
 
+	if (PIT_FLAG_GET(PIT_CH2))
+	{
+		// 通道2发生中断
+		pit_flag_clear(PIT_CH2);
+	}
+
+	if (PIT_FLAG_GET(PIT_CH3))
+	{
+		// 通道3发生中断
+		pit_flag_clear(PIT_CH3);
+	}
+
+	__DSB();
+}
